5e17e8032ed0f96660d2803f7fdf6b1630344377
unknown <constantine61732838250@gmail.com>
2020-02-26 11:57:57 +0800

    fix RWD

---
+++ src/components/Pagination.vue
@@ -0,0 +1,33 @@
+<template>
+    <nav aria-label="Page navigation">
+        <ul class="pagination">
+            <li class="page-item disabled">
+                <a class="page-link" href="#" tabindex="-1">Previous</a>
+            </li>
+            <li :class="['page-item', pageActive]" v-for="page in allPages" :key="page">
+                <a class="page-link">{{page}}</a>
+            </li>
+            <li class="page-item">
+                <a class="page-link" href="#">Next</a>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'TcsPagination',
+    props: ['dataRecordsLength'],
+    data () {
+        return {
+            currentPage: 1,
+            pageActive: 'active'
+        }
+    },
+    computed: {
+        allPages() {
+            return Math.floor(this.dataRecordsLength / 10) + 1;
+        }
+    }
+}
+</script>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,14 +1,22 @@
 <template>
-    <nav class="navbar navbar-inverse">
-        <div class="container-fluid">
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
             <div class="navbar-header">
                 <router-link :to="{ name: 'index' }" class="navbar-brand">T.C.S Hair</router-link>
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapsibleNavbar" aria-expanded="false" aria-controls="collapsibleNavbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
             </div>
-            <ul class="nav navbar-nav navbar-right">
-                <li><span class="navbar-brand">{{today | dateFormat}}</span></li>
-                <li><router-link :to="{ name: 'CustomerInfo' }" class="navbar-brand"><i class="fa fa-edit"></i> 顧客消費</router-link></li>
-                <li><router-link :to="{ name: 'AssistantSalary' }" class="navbar-brand"><i class="fa fa-money"></i> 員工薪資</router-link></li>
-            </ul>
+            <div class="navbar-collapse collapse in" id="collapsibleNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><span class="navbar-brand">{{today | dateFormat}}</span></li>
+                    <li><router-link :to="{ name: 'CustomerInfo' }" class="navbar-brand"><i class="fa fa-edit"></i> 顧客消費</router-link></li>
+                    <li><router-link :to="{ name: 'AssistantSalary' }" class="navbar-brand"><i class="fa fa-money"></i> 員工薪資</router-link></li>
+                </ul>
+            </div>
         </div>
     </nav>
 </template>
